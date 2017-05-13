15ddfda00e255dd3b8c63dd9e9a714f739c24c8f
tyln <taylan.sbrcn@gmail.com>
2017-05-13 14:34:00 +0300

    added parameter generation

--- piri-annotation/src/main/java/com/raqun/PiriActivity.java
+++ piri-annotation/src/main/java/com/raqun/PiriActivity.java
@@ -5,7 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+
 @Retention(RetentionPolicy.CLASS)
 @Target(ElementType.TYPE)
 public @interface PiriActivity {
+    Class<?>[] types() default {};
+
+    String[] names() default {};
 }
--- piri-processor/src/main/java/com/raqun/PiriProcessor.java
+++ piri-processor/src/main/java/com/raqun/PiriProcessor.java
@@ -11,7 +11,6 @@ import java.util.List;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
-import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.lang.model.SourceVersion;
@@ -19,23 +18,23 @@ import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.MirroredTypesException;
 import javax.tools.Diagnostic;
 
 @SupportedAnnotationTypes("com.raqun.PiriActivity")
 public final class PiriProcessor extends AbstractProcessor {
 
+    //TODO remove static package name implementation
+    private static final String PACKAGE_NAME = "com.raqun.piri.sample";
+
     private static final ClassName intentClass = ClassName.get("android.content", "Intent");
     private static final ClassName contextClass = ClassName.get("android.content", "Context");
+
     private static final String METHOD_PREFIX_NEW_INTENT = "newIntentFor";
+    private static final String PARAM_NAME_CONTEXT = "context";
 
-    private List<MethodSpec> newIntentMethodSpecs;
+    private List<MethodSpec> newIntentMethodSpecs = new ArrayList<>();
     private boolean HALT = false;
-
-    @Override
-    public synchronized void init(ProcessingEnvironment processingEnvironment) {
-        super.init(processingEnvironment);
-        this.newIntentMethodSpecs = new ArrayList<>();
-    }
 
     @Override
     public SourceVersion getSupportedSourceVersion() {
@@ -44,13 +43,9 @@ public final class PiriProcessor extends AbstractProcessor {
 
     @Override
     public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
-        final Set<? extends Element> elements = roundEnvironment.getElementsAnnotatedWith(PiriActivity.class);
-        for (Element element : elements) {
-            if (element.getKind() != ElementKind.CLASS) {
-                processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "PiriActivity can only use for classes!");
-                return HALT;
-            }
-            generateNewIntentMethod((TypeElement) element);
+
+        if (!processAnnotations(roundEnvironment)) {
+            return HALT;
         }
 
         if (roundEnvironment.processingOver()) {
@@ -65,16 +60,62 @@ public final class PiriProcessor extends AbstractProcessor {
         return HALT;
     }
 
-    private void generateNewIntentMethod(TypeElement element) {
-        final MethodSpec navigationMethodSpec = MethodSpec
+    private boolean processAnnotations(RoundEnvironment roundEnv) {
+        final Set<? extends Element> elements = roundEnv.getElementsAnnotatedWith(PiriActivity.class);
+        for (Element element : elements) {
+            if (element.getKind() != ElementKind.CLASS) {
+                processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "PiriActivity can only be used for classes!");
+                return false;
+            }
+
+            if (!generateNewIntentMethod((TypeElement) element)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean generateNewIntentMethod(TypeElement element) {
+
+        final PiriActivity annotation = element.getAnnotation(PiriActivity.class);
+
+        if (annotation == null) {
+            processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "Annotation cannot be null!");
+            return false;
+        }
+
+        Class[] types = null;
+        String[] keys = null;
+
+        try {
+            types = annotation.types();
+            keys = annotation.names();
+            
+        } catch (MirroredTypesException ex) {
+
+            // TODO write an exception handler for this
+            processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "ops stg went wrong!");
+        }
+
+        if (keys.length != types.length) {
+            processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "You must specify a parameter type for eacy key!");
+            return false;
+        }
+
+        final MethodSpec.Builder navigationMethodSpecBuilder = MethodSpec
                 .methodBuilder(METHOD_PREFIX_NEW_INTENT + element.getSimpleName())
                 .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
-                .addParameter(contextClass, "context")
                 .returns(intentClass)
-                .addStatement("return new $T($L, $L)", intentClass, "context", element.getQualifiedName() + ".class")
-                .build();
+                .addStatement("return new $T($L, $L)", intentClass, PARAM_NAME_CONTEXT, element.getQualifiedName() + ".class")
+                .addParameter(contextClass, PARAM_NAME_CONTEXT);
 
-        newIntentMethodSpecs.add(navigationMethodSpec);
+        for (int i = 0; i < keys.length; i++) {
+            navigationMethodSpecBuilder.addParameter(types[i], keys[i]);
+        }
+
+        newIntentMethodSpecs.add(navigationMethodSpecBuilder.build());
+        return true;
     }
 
     private void generateNavigator() throws IOException {
@@ -85,7 +126,7 @@ public final class PiriProcessor extends AbstractProcessor {
         }
 
         final TypeSpec piriSpec = builder.build();
-        JavaFile.builder("com.raqun.piri.sample", piriSpec)
+        JavaFile.builder(PACKAGE_NAME, piriSpec)
                 .build()
                 .writeTo(processingEnv.getFiler());
     }
